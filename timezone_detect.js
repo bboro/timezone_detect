74b2882e4c0817399169179546d54e1bf78acaaf
JordanMagnuson <jm.magnuson@gmail.com>
2013-07-12 12:31:06 -0600

    Issue #2026153 by milesw: Automatically keep user's timezone updated.

--- timezone_detect.js
+++ timezone_detect.js
@@ -7,12 +7,15 @@ jQuery(document).ready(function () {
   // Determine timezone from browser client using jsTimezoneDetect library.
   var tz = jstz.determine();
 
-  // Post timezone to callback url via ajax.
-  jQuery.ajax({
-    type: 'POST',
-    url: '/timezone-detect/set-timezone',
-    dataType: 'json',
-    data: 'timezone=' + tz.name()
-  });
+  if (tz.name() != Drupal.settings.timezone_detect.current_timezone) {
+
+    // Post timezone to callback url via ajax.
+    jQuery.ajax({
+      type: 'POST',
+      url: '/timezone-detect/set-timezone',
+      dataType: 'json',
+      data: 'timezone=' + tz.name()
+    });
+  }
 
 });
