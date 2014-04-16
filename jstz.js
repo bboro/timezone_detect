5b6c80e9ff681816439a3f912f6c404333cc83a2
Jordan Magnuson <jm.magnuson@gmail.com>
2014-04-16 09:14:22 -0700

    Decided to go ahead and apply Drupal coding standards to jstz.

--- jstz.js
+++ jstz.js
@@ -103,7 +103,7 @@
               var key = lookup_key();
               return new jstz.TimeZone(jstz.olson.timezones[key]);
           },
-          
+
           /**
            * This object contains information on when daylight savings starts for
            * different timezones.
@@ -116,7 +116,8 @@
            */
           dst_start_for = function (tz_name) {
 
-            var ru_pre_dst_change = new Date(2010, 6, 15, 1, 0, 0, 0), // In 2010 Russia had DST, this allows us to detect Russia :)
+            // In 2010 Russia had DST, this allows us to detect Russia :)
+            var ru_pre_dst_change = new Date(2010, 6, 15, 1, 0, 0, 0),
                 dst_starts = {
                     'America/Denver':       new Date(2011, 2, 13, 3, 0, 0, 0),
                     'America/Mazatlan':     new Date(2011, 3, 3, 3, 0, 0, 0),
@@ -162,7 +163,7 @@
       return {
           determine: determine,
           date_is_dst: date_is_dst,
-          dst_start_for: dst_start_for 
+          dst_start_for: dst_start_for
       };
   }());
 
@@ -205,7 +206,7 @@
           },
 
           timezone_name = tz_name,
-          
+
           /**
            * Checks if a timezone has possible ambiguities. I.e timezones that are similar.
            *
