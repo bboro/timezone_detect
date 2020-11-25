f5aa8d6d534bb2428f7eddbe47eec5808879d5d3
Bysa Boro <baysaa@gmail.com>
2020-11-25 21:08:04 +0000

    Modify the JS to prepare to add a dialog option

--- timezone_detect.js
+++ timezone_detect.js
@@ -4,32 +4,50 @@
  *
  * @todo: Use Drupal.behaviors?
  */
-(function ($, Drupal, drupalSettings) {
+(function ($, Drupal, drupalSettings, jstz) {
 
-  $(document).ready(function () {
+  var behavior = Drupal.behaviors.timezoneDetect = {};
 
+  behavior.attach = function () {
     // Determine timezone from browser client using jsTimezoneDetect library.
     var tz = jstz.determine();
 
     if (tz.name() != drupalSettings.timezone_detect.current_timezone) {
-
-      // Post timezone to callback url via ajax.
-      $.ajax({
-        type: 'POST',
-        url: drupalSettings.path.baseUrl + 'timezone-detect/ajax/set-timezone',
-        dataType: 'json',
-        data: {
-          timezone: tz.name(),
-          token: drupalSettings.timezone_detect.token
-        }
-      });
-
-      // Set any timezone select on this page to the detected timezone.
-      $('select[name="timezone"] option[value="' + tz.name() + '"]')
-        .closest('select')
-        .val(tz.name());
+      behavior.showDialog(tz);
     }
+  }
 
-  });
+  behavior.showDialog = function (tz) {
+    var dialog = Drupal.dialog(Drupal.t('Would you like to change your timezone from @existing to @current?', {'@current': tz.name(), '@existing': drupalSettings.timezone_detect.current_timezone}), {
+      dialogClass: 'confirm-dialog',
+      resizable: false,
+      closeOnEscape: false,
+      width:600,
+      title:"do you want to publish this content ?",
+      create: function () {
+        // $(this).parent().find('.ui-dialog-titlebar-close').remove();
+      },
+      beforeClose: false,
+      close: function (event) {
+        $(event.target).remove();
+      }
+    });
 
-})(jQuery, Drupal, drupalSettings);
+    // Post timezone to callback url via ajax.
+    $.ajax({
+      type: 'POST',
+      url: drupalSettings.path.baseUrl + 'timezone-detect/ajax/set-timezone',
+      dataType: 'json',
+      data: {
+        timezone: tz.name(),
+        token: drupalSettings.timezone_detect.token
+      }
+    });
+
+    // Set any timezone select on this page to the detected timezone.
+    $('select[name="timezone"] option[value="' + tz.name() + '"]')
+      .closest('select')
+      .val(tz.name());
+  }
+
+})(jQuery, Drupal, window.drupalSettings, window.jstz);
